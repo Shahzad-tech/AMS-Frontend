ef96b37cf5569aa9c2c800f790e073c64954fc81
Ali Arshad <ali.arshad@synergy-it.com>
2023-06-19 15:42:42 +0500

    Boilerplate Added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    importProvidersFrom([
+      BrowserModule,
+      BrowserAnimationsModule
+    ]),
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules)
+    ),
+    provideAnimations(),
+    provideHttpClient()
+  ]
 };
