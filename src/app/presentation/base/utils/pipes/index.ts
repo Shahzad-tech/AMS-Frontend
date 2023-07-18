c2df6e4203ea72a5cd5bfa20062a49a559272956
Shahzad Javaid <shahzad.javaid@synergy-it.com>
2023-07-18 14:36:53 +0500

    Auto-generated commit [ci skip]

--- src/app/presentation/base/utils/pipes/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./luxon-date.pipe";
-export * from "./parse-abstract-control.pipe";
-export * from "./parse-abstract-control-to-form-group.pipe";
--- src/app/presentation/base/utils/pipes/luxon-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LuxonDatePipe } from './luxon-date.pipe';
-
-describe('LuxonDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new LuxonDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
